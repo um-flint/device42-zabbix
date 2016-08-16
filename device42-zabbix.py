bf462183550dfb5b33a7c60e9dfb08741f5c2a35
Mark Mercado <mamercad@umflint.edu>
2016-08-16 10:17:50 -0400

    ugh, 'os' is clashing when unset i think

--- device42-zabbix.py
+++ device42-zabbix.py
@@ -76,43 +76,48 @@ for device in devices['Devices']:
         zabbix_hostid = res['result'][0]['hostid']
         print "  Zabbix device name (id): {} ({})".format(zabbix_hostname, zabbix_hostid)
 
-        software       = device['os']
-        software_app_a = device['custom_fields'][0]['key'] + ': ' + device['custom_fields'][0]['value']
-        software_app_b = device['custom_fields'][1]['key'] + ': ' + device['custom_fields'][1]['value']
-        software_app_c = device['custom_fields'][2]['key'] + ': ' + device['custom_fields'][2]['value']
-        software_app_d = device['custom_fields'][3]['key'] + ': ' + device['custom_fields'][3]['value']
+        try:
+            software       = device['os']
+            software_app_a = device['custom_fields'][0]['key'] + ': ' + device['custom_fields'][0]['value']
+            software_app_b = device['custom_fields'][1]['key'] + ': ' + device['custom_fields'][1]['value']
+            software_app_c = device['custom_fields'][2]['key'] + ': ' + device['custom_fields'][2]['value']
+            software_app_d = device['custom_fields'][3]['key'] + ': ' + device['custom_fields'][3]['value']
 
-        print "    Setting inventory 'software' -> {}".format(software)
-        print "    Setting inventory 'software_app_a' -> {}".format(software_app_a)
-        print "    Setting inventory 'software_app_b' -> {}".format(software_app_b)
-        print "    Setting inventory 'software_app_c' -> {}".format(software_app_c)
-        print "    Setting inventory 'software_app_d' -> {}".format(software_app_d)
+            print "    Setting inventory 'software' -> {}".format(software)
+            print "    Setting inventory 'software_app_a' -> {}".format(software_app_a)
+            print "    Setting inventory 'software_app_b' -> {}".format(software_app_b)
+            print "    Setting inventory 'software_app_c' -> {}".format(software_app_c)
+            print "    Setting inventory 'software_app_d' -> {}".format(software_app_d)
 
-        zabbix_inventory = {
-            "jsonrpc": "2.0",
-            "method": "host.update",
-            "params": {
-                "hostid": zabbix_hostid,
-                "inventory_mode": 1, # automatic
-                "inventory": {
-                    "software": software,
-                    "software_app_a": software_app_a,
-                    "software_app_b": software_app_b,
-                    "software_app_c": software_app_c,
-                    "software_app_d": software_app_d
-                }
-            },
-            "auth": zabbix_key,
-            "id": 1
-        }
-        r = requests.post(config.get('ZABBIX', 'apiurl'), json=zabbix_inventory)
+            zabbix_inventory = {
+                "jsonrpc": "2.0",
+                "method": "host.update",
+                "params": {
+                    "hostid": zabbix_hostid,
+                    "inventory_mode": 1, # automatic
+                    "inventory": {
+                        "software": software,
+                        "software_app_a": software_app_a,
+                        "software_app_b": software_app_b,
+                        "software_app_c": software_app_c,
+                        "software_app_d": software_app_d
+                    }
+                },
+                "auth": zabbix_key,
+                "id": 1
+            }
+            r = requests.post(config.get('ZABBIX', 'apiurl'), json=zabbix_inventory)
 
-        if r.status_code != 200:
-            print r.status_code
-            print r.text
+            if r.status_code != 200:
+                print r.status_code
+                print r.text
+                failed.append(d42_name)
+            else:
+                updated.append(d42_name)
+
+        except Exception as e:
+            print "Exception: {}".format(e)
             failed.append(d42_name)
-        else:
-            updated.append(d42_name)
 
     else:
         print "  Not found in Zabbix"
